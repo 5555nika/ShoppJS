59d1078e41dc50f40b30af5c6ea9eb791a64defa
5555nika <niiarasisman@gmail.com>
2026-06-23 12:47:43 +0300

    Updates

---
+++ assets/basket-BMG5E2Nc.js
@@ -0,0 +1,22 @@
+import{a as e,i as t,n,o as r,r as i,t as a}from"./utils-De9S9yiS.js";var o=document.querySelector(`.cart`),s=[];o?.addEventListener(`click`,u),c();async function c(){try{if(!s.length){let e=await fetch(`/products.json`);if(!e.ok)throw Error(e.statusText);l(await e.json())}}catch(n){t(e),console.log(n instanceof Error?n.message:`error`)}}function l(i){if(!o)return;if(o.innerHTML=``,!i||!i.length){t(e);return}a(i);let s=n();if(!s||!s.length){t(r);return}let c=i.filter(e=>s.includes(e.id));if(!c){t(r);return}d(c)}function u(e){let t=e.target.closest(`.cart__del-cart`);if(!t)return;let r=t.closest(`.cart__product`),a=Number(r?.dataset?.productId),o=n();o=o.filter(e=>e!==a),i(o),c()}function d(e){o&&e.forEach(e=>{let{id:t,img:n,title:r,price:i,discount:a}=e,s=`
+            <div class="cart__product" data-product-id="${t}">
+                <div class="cart__img">
+                    <img src="${n}" alt="${r}">
+                </div>
+                <div class="cart__info">
+                    <div class="cart__title">${r}</div>
+                    <div class="cart__block-btns">
+                        <div class="cart__minus">-</div>
+                        <div class="cart__count">1</div>
+                        <div class="cart__plus">+</div>
+                    </div>
+                </div>
+                <div class="cart__prices">
+                    <div class="cart__price"><span>${i}</span> руб</div>
+                    <div class="cart__price-discount"><span>${Math.round(i-i*a/100)}</span> руб</div>
+                </div>
+                <div class="cart__del-cart">
+                    <i class="bi bi-trash3"></i>
+                </div>
+            </div>
+        `;o.insertAdjacentHTML(`beforeend`,s)})}
---
+++ assets/card-Bwmtjy7E.js
@@ -0,0 +1,17 @@
+import{a as e,i as t,s as n,t as r}from"./utils-De9S9yiS.js";var i=document.querySelector(`.wrapper`),a=[];o();async function o(){try{if(!a.length){let e=await fetch(`/products.json`);if(!e.ok)throw Error(e.statusText);c(await e.json())}}catch(n){t(e),console.log(n instanceof Error?n.message:`error`)}}function s(e){return new URLSearchParams(window.location.search).get(e)}function c(i){if(!i||i.length===0){t(e);return}r(i);let a=Number(s(`id`));a||t(n);let o=i.find(e=>e.id===a);if(!o){t(n);return}l(o)}function l(e){let{img:t,title:n,price:r,discount:a,descr:o}=e,s=`
+        <div class="product">
+            <h2 class="product__title">${n}</h2>
+                <div class="product__img">
+                    <img src="${t}" alt="${n}">
+                </div>
+                <p class="product__descr">${o}</p>
+                    <div class="product__inner-price">
+                        <div class="product__price">
+                            <b>Цена:</b> ${r} руб
+                        </div>
+                        <div class="product__price--discount">
+                            <b>Цена со скидкой:</b> ${Math.round(r-r*a/100)} руб
+                        </div>
+                    </div>
+        </div>
+    `;i&&i.insertAdjacentHTML(`beforeend`,s)}
